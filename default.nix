54a9b0ce5c7fb2ff3aaeea2facb9d4d6092b708d
Peter Jones <pjones@devalot.com>
2018-10-01 13:28:09 -0700

    Update the build script based on recent changes to Edify

--- default.nix
+++ default.nix
@@ -1,92 +1,40 @@
+{
+}@args:
+
 let
-  # All dependencies come from this package source:
-  pinned = {
-    owner  = "NixOS";
-    repo   = "nixpkgs-channels";
-
-    # NixOS 17.09 (stable) as of 1/30/2018:
-    rev    = "c2b668ee7264000b10d554754150ea9305b6ef3d";
-    sha256 = "1nqjsl163dahghidh06k3ynzq6kkmb9pzj1njw6ilfx7axygkyx8";
+  repo = fetchGit {
+    url = "git://git.devalot.com/edify.git";
+    rev = "5faef56f9d28dd8c1233d37edd7cd72af1fc03a2";
   };
 
-  # Which version of Edify to use:
-  edifyRepo = {
-    url    = "git://git.devalot.com/edify.git";
-    rev    = "c68b642890801b8ae4390ab1025f4488f1c59762";
-    sha256 = "0v30y6ia3mcqfd29mfzrik4jfgb2m0rrjb046cw86rfiqsg8dsaq";
-  };
-
-  # Load the host's nixpkgs, then the pinned version:
-  hostPkgs   = import <nixpkgs> {};
-  pinnedPkgs = import (hostPkgs.fetchFromGitHub pinned) {};
-  edifyPkg   = import (pinnedPkgs.fetchgit edifyRepo) { pkgs = pinnedPkgs; };
-
+  edify = import "${repo}/nix/builder.nix" args;
+  pkgs  = edify.pkgs;
 in
-{ pkgs ? pinnedPkgs
-}:
 
-pkgs.stdenv.mkDerivation rec {
+edify.mkDerivation rec {
   name = "gitclass-${version}";
   version = "1.4";
   src = ./.;
 
-  # Specifically don't want fixups for this package:
-  phases = [ "unpackPhase" "buildPhase" "installPhase" ];
-
   # Tell TeX where we keep STY files:
   TEXINPUTS = "${src}/vendor/gitdags:";
 
+  # Extra files to install:
+  extraFiles = [ "LICENSE" "README.md" "examples" "repos" ];
+
   # Additional system dependencies:
   buildInputs = with pkgs; [
-    inkscape # For SVG -> PDF
-    graphviz_2_32 # For DOT -> PDF
-    mscgen # For MSC -> PDF
-
-    # Markdown -> PDF:
-    edifyPkg
-    pandoc
-    haskellPackages.pandoc-citeproc
-    haskellPackages.pandoc-crossref
-
-    # TeX:
-    (texlive.combine {
-      inherit (texlive) scheme-small collection-binextra beamer
-        xcolor-solarized pgf standalone;
-    })
-
     # For running git commands during the build
     gitMinimal
-
-    # For packaging:
-    zip
   ];
 
+  # Generate example repositories:
   buildPhase = ''
     "$SHELL" scripts/repo-all.sh
-    edify build --top "$(pwd)" courses/*.md
   '';
 
+  # Extra install steps:
   installPhase = ''
-    dest=$out/${name}
-
-    mkdir -p $dest/handouts -p $dest/slides
-    cp -rp LICENSE README.md $dest/
-
-    # Copy PDF files into the correct locations:
-    find build -type f -name '*.handout.pdf' -exec cp '{}' $dest/handouts ';'
-    find build -type f -name '*.slides.pdf'  -exec cp '{}' $dest/slides ';'
-
-    # Rename PDF files:
-    for file in $dest/{handouts,slides}/*.pdf; do
-      mv $file $(echo $file | sed -E 's/[.](handout|slides)[.]pdf/.pdf/')
-    done
-
-    # Copy assets needed by the students:
-    cp -rp examples repos $dest
-
-    # Build archives:
-    ( cd $out && zip -9 -y -r -q ${name}.zip ${name} )
-
     # Link archive to generic name (for the NixOS container I use):
     ( cd $out && ln -nfs ${name}.zip gitclass.zip )
   '';
